0a9b9047e0ee4e0c06592ed312ec232888005d76
Mamadou Sy <mamadoujuniorsy@gmail.com>
2023-06-25 01:34:12 +0000

    version 1 de notre plateforme de detection de fake_news

---
+++ app.py
@@ -0,0 +1,277 @@
+from flask import Flask, render_template, request, redirect, session, flash,url_for
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import Form, StringField, PasswordField, validators
+from flask_login import LoginManager, current_user, UserMixin, login_user, logout_user, login_required
+from models.model import predict_fake_news
+import re
+
+
+app = Flask(__name__, static_url_path='/static')
+
+app.config['SECRET_KEY'] = 'esp2023@'  # Clé secrète pour sécuriser les sessions
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'fake_news'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+login_manager = LoginManager(app)
+
+# Définir une classe User personnalisée en utilisant UserMixin de Flask-Login
+class User(UserMixin):
+     def __init__(self, user_id, firstname, lastname,password,email,is_admin):
+        self.id = user_id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.password = password
+        self.email = email
+        self.is_admin = is_admin
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Implémentez cette fonction pour charger et renvoyer l'utilisateur à partir de l'ID d'utilisateur (user_id)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+
+    if user:
+        user_obj = User(user['id'], user['firstname'], user['lastname'], user['password'], user['email'], user['is_admin'])
+        return user_obj
+    else:
+        return None
+
+# Classe du formulaire d'inscription
+class SignupForm(Form):
+    firstname = StringField('Prénom', [validators.DataRequired()])
+    lastname = StringField('Nom', [validators.DataRequired()])
+    email = StringField('Adresse Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Mot de passe', [
+        validators.DataRequired(),
+        validators.Length(min=6)
+    ])
+
+# Classe du formulaire de connexion
+class LoginForm(Form):
+    email = StringField('Adresse Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Mot de passe', [validators.DataRequired()])
+
+# Page d'accueil / Inscription
+@app.route('/')
+def home():
+    return render_template('acceuil.html')
+
+@app.route('/static')
+def serve_css():
+    return app.send_static_file('style.css')
+
+# Page d'utilisateur
+@app.route('/user', methods=['GET', 'POST'])
+@login_required
+def user():
+    username = current_user.firstname + " " + current_user.lastname
+    result = None
+
+    if request.method == 'POST':
+        link = request.form['link']
+        result = predict_fake_news(link)
+    return render_template('user.html', username=username, result=result)
+
+# Page de déconnexion
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+# Page d'inscription
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm(request.form)
+    if request.method == 'POST' and form.validate():
+        firstname = form.firstname.data
+        lastname = form.lastname.data
+        email = form.email.data
+        password = form.password.data
+
+        # Vérifier si l'utilisateur existe déjà dans la base de données
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+        user = cur.fetchone()
+        if user:
+            error = 'Cet utilisateur existe déjà.'
+            return render_template('signup.html', form=form, error=error)
+
+        # Créer un nouvel utilisateur et l'ajouter à la base de données
+        cur.execute("INSERT INTO users (firstname, lastname, email, password) VALUES (%s, %s, %s, %s)",
+                    (firstname, lastname, email, generate_password_hash(password)))
+        mysql.connection.commit()
+        cur.close()
+
+        # Rediriger vers la page de connexion après l'inscription réussie
+        return redirect('/login')
+
+    return render_template('signup.html', form=form)
+
+# Page de connexion
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        email = form.email.data
+        password = form.password.data
+
+        # Vérifier les informations de connexion de l'utilisateur dans la base de données
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
+        user = cur.fetchone()
+
+        if user and check_password_hash(user['password'], password):
+            # Les informations d'identification sont correctes, connectez l'utilisateur
+            user_obj = User(user['id'], user['firstname'], user['lastname'], user['password'], user['email'], user['is_admin'])
+            login_user(user_obj)
+            if user_obj.is_admin:
+                return redirect(url_for('admin'))
+            else:
+                return redirect(url_for('user'))
+        else:
+            # Les informations d'identification sont incorrectes, afficher un message d'erreur
+            error = 'Adresse email ou mot de passe incorrect.'
+            return render_template('login.html', form=form, error=error)
+
+    return render_template('login.html', form=form)
+@app.route('/update_account', methods=['GET', 'POST'])
+@login_required
+def update_account():
+    form = SignupForm(request.form)
+    if request.method == 'POST' and form.validate():
+        firstname = form.firstname.data
+        lastname = form.lastname.data
+        email = form.email.data
+        password = form.password.data
+
+        # Vérifier si un nouveau mot de passe a été saisi
+        if password:
+            hashed_password = generate_password_hash(password)
+        else:
+            hashed_password = current_user.password
+
+        # Mettre à jour les informations de l'utilisateur dans la base de données
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE users SET firstname = %s, lastname = %s, email = %s, password = %s WHERE id = %s",
+                    (firstname, lastname, email, hashed_password, current_user.id))
+        mysql.connection.commit()
+        cur.close()
+
+        # Rediriger vers la page de profil de l'utilisateur avec un message de succès
+        flash('Les informations du compte ont été mises à jour avec succès', 'success')
+        return redirect('/user')
+
+    return render_template('update_account.html', form=form)
+# Page d'administration
+@app.route('/admin')
+@login_required
+def admin():
+    if current_user.is_admin:
+        # Récupérer la liste des utilisateurs depuis la base de données
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users")
+        users = cur.fetchall()
+        cur.close()
+
+        return render_template('admin/admin.html', users=users)
+    else:
+        flash('Accès refusé. Vous n\'êtes pas administrateur.', 'error')
+        return redirect('/')
+
+# Page de modification d'utilisateur
+@app.route('/admin/edit/<int:user_id>', methods=['GET', 'POST'])
+@login_required
+def edit_user(user_id):
+    if current_user.is_admin:
+        # Récupérer les informations de l'utilisateur à partir de l'ID
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+        user = cur.fetchone()
+        cur.close()
+
+        if user:
+            # Afficher le formulaire de modification avec les informations de l'utilisateur
+            form = SignupForm(request.form)
+            form.firstname.data = user['firstname']
+            form.lastname.data = user['lastname']
+            form.email.data = user['email']
+
+            if request.method == 'POST' and form.validate():
+                # Mettre à jour les informations de l'utilisateur dans la base de données
+                firstname = form.firstname.data
+                lastname = form.lastname.data
+                email = form.email.data
+
+                cur = mysql.connection.cursor()
+                cur.execute("UPDATE users SET firstname = %s, lastname = %s, email = %s WHERE id = %s",
+                            (firstname, lastname, email, user_id))
+                mysql.connection.commit()
+                cur.close()
+
+                flash('Utilisateur mis à jour avec succès.', 'success')
+                return redirect('/admin')
+            
+            return render_template('admin/edit_user.html', form=form, user_id=user_id)
+        else:
+            flash('Utilisateur non trouvé.', 'error')
+            return redirect('/admin')
+    else:
+        flash('Accès refusé. Vous n\'êtes pas administrateur.', 'error')
+        return redirect('/')
+
+# Page de suppression d'utilisateur
+@app.route('/admin/delete/<int:user_id>')
+@login_required
+def delete_user(user_id):
+    if current_user.is_admin:
+        # Supprimer l'utilisateur de la base de données
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM users WHERE id = %s", (user_id,))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Utilisateur supprimé avec succès.', 'success')
+        return redirect('/admin')
+    else:
+        flash('Accès refusé. Vous n\'êtes pas administrateur.', 'error')
+        return redirect('/')
+
+# Page de création d'utilisateur
+@app.route('/admin/create', methods=['GET', 'POST'])
+@login_required
+def create_user():
+    if current_user.is_admin:
+        form = SignupForm(request.form)
+
+        if request.method == 'POST' and form.validate():
+            # Créer un nouvel utilisateur et l'ajouter à la base de données
+            firstname = form.firstname.data
+            lastname = form.lastname.data
+            email = form.email.data
+            password = form.password.data
+
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO users (firstname, lastname, email, password) VALUES (%s, %s, %s, %s)",
+                        (firstname, lastname, email, generate_password_hash(password)))
+            mysql.connection.commit()
+            cur.close()
+
+            flash('Nouvel utilisateur créé avec succès.', 'success')
+            return redirect('/admin')
+
+        return render_template('admin/create_user.html', form=form)
+    else:
+        flash('Accès refusé. Vous n\'êtes pas administrateur.', 'error')
+        return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/model.py
@@ -0,0 +1,132 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Téléchargement des ressources nécessaires de NLTK
+nltk.download('stopwords')
+# Fonction de prétraitement du texte
+def preprocess_text(text):
+    # Suppression des caractères spéciaux et des chiffres
+    text = re.sub(r'\W+|\d+', ' ', text)
+
+    # Mise en minuscules
+    text = text.lower()
+
+    # Tokenisation
+    tokens = text.split()
+
+    # Suppression des mots vides (stopwords) français
+    stop_words = set(stopwords.words('french'))
+    tokens = [word for word in tokens if word not in stop_words]
+     # Reconstitution du texte prétraité
+    preprocessed_text = ' '.join(tokens)
+
+    return preprocessed_text
+
+    # Stemming
+    stemmer = SnowballStemmer('french')
+    tokens = [stemmer.stem(word) for word in tokens]
+
+
+# Charger les données d'entraînement depuis le fichier CSV
+data = pd.read_csv("datafake_train.csv", header=0, delimiter=";")
+
+# Prétraitement du texte
+data['post'] = data['post'].apply(preprocess_text)
+
+# Extraction des caractéristiques avec TF-IDF
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(data["post"])
+
+# Extraction des étiquettes
+y = data["fake"]
+
+# Division des données en ensembles d'entraînement et de validation
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Entraînement du modèle SVM
+svm_model = SVC()
+svm_model.fit(X_train, y_train)
+
+# Entraînement du modèle de forêt aléatoire
+rf_model = RandomForestClassifier()
+rf_model.fit(X_train, y_train)
+
+# Validation croisée
+svm_val_preds = svm_model.predict(X_val)
+svm_val_accuracy = accuracy_score(y_val, svm_val_preds)
+
+rf_val_preds = rf_model.predict(X_val)
+rf_val_accuracy = accuracy_score(y_val, rf_val_preds)
+
+def predict_fake_news(article_text, threshold=80):
+    # Prétraitement de l'article
+    preprocessed_text = preprocess_text(article_text)
+    
+    # Extraction des caractéristiques avec TF-IDF
+    article_features = vectorizer.transform([preprocessed_text])
+    
+    # Prédiction avec les modèles
+    svm_prediction = svm_model.predict(article_features)
+    rf_prediction = rf_model.predict(article_features)
+    
+    # Combinaison des prédictions
+    combined_prediction = (svm_prediction + rf_prediction) / 2
+    
+    # Calcul du pourcentage de chance que l'information soit fausse
+    fake_percentage = 100 - (combined_prediction * 100)
+    
+    # Vérification du seuil
+    if fake_percentage >= threshold:
+        message = "L'article est très probablement un fake news."
+    else:
+        message = "L'article semble être authentique."
+    
+    return message    
+# Exemple d'utilisation
+#article = ""
+#result = predict_fake_news(article)
+#print(result)
+#print("Validation Accuracy - SVM:", svm_val_accuracy)
+#print("Validation Accuracy - Random Forest:", rf_val_accuracy)
+
+# Charger les données de test depuis le fichier CSV
+#test_data = pd.read_csv("datafake_test.csv", header=0, delimiter=";")
+
+# Prétraitement du texte pour les données de test
+#test_data['post'] = test_data['post'].apply(preprocess_text)
+
+# Prétraitement des données de test
+#X_test = vectorizer.transform(test_data["post"])
+
+# Prédiction des étiquettes des données de test
+#svm_test_preds = svm_model.predict(X_test)
+#rf_test_preds = rf_model.predict(X_test)
+
+# Affichage des prédictions pour SVM
+#print("Prédictions SVM :")
+#for i, prediction in enumerate(svm_test_preds):
+ #   print("Post:", test_data["post"][i])
+  #  if prediction == 0:
+   #     print("Prédiction: Fausse")
+    #else:
+     #   print("Prédiction: Vraie")
+    #print("---------------------------")
+
+# Affichage des prédictions pour Random Forest
+#print("Prédictions Random Forest :")
+#for i, prediction in enumerate(rf_test_preds):
+ #   print("Post:", test_data["post"][i])
+  #  if prediction == 0:
+   #     print("Prédiction: Fausse")
+    #else:
+     #   print("Prédiction: Vraie")
+    #print("---------------------------")
